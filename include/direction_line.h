d9458557eca07a421addc2407893db708516fc4e
Saad Sadiq <ssadiq8@illinois.edu>
2021-05-04 23:55:33 -0500

    Added javadocs, tests, comments + README.md

--- include/direction_line.h
+++ include/direction_line.h
@@ -9,11 +9,34 @@ namespace finalproject {
     class DirectionLine {
 
     public:
+        /**
+         * Default constructor
+         */
         DirectionLine();
+        /**
+         * Constructor using a start and end point
+         * @param start_point starting from ball
+         * @param end_point dragged
+         */
         DirectionLine(vec2 start_point, vec2 end_point);
+        /**
+         * Gets magnitude of line
+         * @return magnitude
+         */
         float GetMagnitude();
+        /**
+         * cosine of angle between ball and line
+         * @return sine of angle between ball and line
+         */
         float CosAngle();
+        /**
+         * sine of angle between ball and line
+         * @return sine of angle between ball and line
+         */
         float SinAngle();
+        /**
+         * Draw direction line
+         */
         void DrawDirectionLine();
         void SetEndPoint(vec2 end_point);
         void MoveLineRight();
